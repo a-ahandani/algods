76a81a89c4c9e657057c277e48ac6a9ebbc4c849
Ahmad <a.e.ahandani@gmail.com>
2023-04-18 00:32:00 -0700

    update tests

--- bubble-sort/bubble-sort.test.ts
+++ bubble-sort/bubble-sort.test.ts
@@ -1,7 +1,28 @@
 import bubbleSort from "./bubble-sort";
 
-test("should be able to sort", () => {
-  expect(bubbleSort([9, 8, 5, 2, 3, 1, 6, 4, 7])).toEqual([
-    1, 2, 3, 4, 5, 6, 7, 8, 9,
-  ]);
+// Test cases for bubbleSort function
+describe("bubbleSort", () => {
+  it("should sort an array of numbers in ascending order", () => {
+    const arr = [5, 3, 8, 4, 1];
+    const expected = [1, 3, 4, 5, 8];
+    expect(bubbleSort(arr)).toEqual(expected);
+  });
+
+  it("should return an empty array if input array is empty", () => {
+    const arr = [];
+    const expected = [];
+    expect(bubbleSort(arr)).toEqual(expected);
+  });
+
+  it("should return the same array if it is already sorted", () => {
+    const arr = [1, 2, 3, 4, 5];
+    const expected = [1, 2, 3, 4, 5];
+    expect(bubbleSort(arr)).toEqual(expected);
+  });
+
+  it("should sort an array of strings in alphabetical order", () => {
+    const arr = ["apple", "banana", "cherry", "date", "fig"];
+    const expected = ["apple", "banana", "cherry", "date", "fig"];
+    expect(bubbleSort(arr)).toEqual(expected);
+  });
 });
--- bubble-sort/bubble-sort.ts
+++ bubble-sort/bubble-sort.ts
@@ -1,4 +1,6 @@
-const bubbleSort = (array: Number[]): Number[] => {
+//Bubble Sort function
+
+const bubbleSort = (array: Number[] | String[]): Number[] | String[] => {
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array.length - i - 1; j++) {
       if (array[j] > array[j + 1]) {
